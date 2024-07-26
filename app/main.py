b9d4d1e7fa6c310fbb2b2241f303957603eaacbe
karthikven <kv771@nyu.edu>
2024-07-26 20:22:06 +0530

    send pong to client

--- app/main.py
+++ app/main.py
@@ -2,10 +2,23 @@ import socket
 
 
 def main():
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
 
+    HOST = "localhost"
+    PORT = 6379
+    server_socket = socket.create_server((HOST, PORT))
+    conn, addr = server_socket.accept()
+    conn.send(b"+PONG\r\n")
+    conn.close()
+        
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # conn, addr = server_socket.accept() # wait for client
+    # print("connected to a client")
+
+    # data = conn.recv(1024)
+    # print("Received from client", data.decode())
+    # conn.send()
+
+    # conn.close()
 
 if __name__ == "__main__":
     main()
