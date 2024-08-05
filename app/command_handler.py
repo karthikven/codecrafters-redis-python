05786ed1e3eec69e837f83bb3eba64e733b04922
karthikven <kv771@nyu.edu>
2024-08-05 10:32:45 +0530

    Implement basic master/slave role distinction with --replicaof flag

--- app/command_handler.py
+++ app/command_handler.py
@@ -3,7 +3,7 @@ from app.resp import deserialize, serialize, SIMPLE_STRING, BULK_STRING, ERROR, 
 from app.store import Store
 import time
 
-def command_dispatcher(data: bytes, store: Store) -> Tuple[bytes, Store]:
+def command_dispatcher(data: bytes, store: Store, replicaof: str) -> Tuple[bytes, Store]:
     """
     function that takes in a request as input, deserializes it, and then dispatches it to the appropriate handler
     """
@@ -15,20 +15,24 @@ def command_dispatcher(data: bytes, store: Store) -> Tuple[bytes, Store]:
         "INFO": handle_info
     }
 
+    # determine if master or replica
+    role = "master"
+    if replicaof:
+        role = "slave"
     # Deserialize the request
     deserialized_data = deserialize(data)
     command = deserialized_data[0]
 
-    return COMMAND_HANDLER_MAP[command](deserialized_data, store)
+    return COMMAND_HANDLER_MAP[command](deserialized_data, store, role)
 
-def handle_ping(data: list, store) -> Tuple[bytes, Store]:
+def handle_ping(data: list, store: Store, role: str) -> Tuple[bytes, Store]:
     return serialize("PONG"), store
 
-def handle_echo(data: list, store: Store) -> Tuple[bytes, Store]:
+def handle_echo(data: list, store: Store, role: str) -> Tuple[bytes, Store]:
     message = data[1]
     return serialize(message), store
 
-def handle_set(data: list, store: Store) -> Tuple[bytes, Store]:
+def handle_set(data: list, store: Store, role: str) -> Tuple[bytes, Store]:
     # get key and value from data
     _, key, value = data[0], data[1], data[2]
     px = None
@@ -37,13 +41,13 @@ def handle_set(data: list, store: Store) -> Tuple[bytes, Store]:
     new_store = store.set(key, value, px)
     return serialize("OK"), new_store
 
-def handle_get(data: list, store: Store) -> Tuple[bytes, Store]:
+def handle_get(data: list, store: Store, role: str) -> Tuple[bytes, Store]:
     _, key = data    
     value = store.get(key)
     return serialize(value if value is not None else None), store 
 
-def handle_info(data: list, store: Store) -> Tuple[bytes, Store]:
+def handle_info(data: list, store: Store, role: str) -> Tuple[bytes, Store]:
     command, _ = data
     if command.lower() == 'info':
-        return serialize("role:master"), store
+        return serialize(f"role:{role}"), store
     return None, store
--- app/main.py
+++ app/main.py
@@ -4,25 +4,28 @@ from app.store import Store
 import argparse
 
 
-async def handle_client(reader, writer):
-    store = Store()
-    while True:
-        data = await reader.read(100)
-        if not data:
-            break
-        to_send, store = command_dispatcher(data, store)
-        writer.write(to_send)
-        await writer.drain()
-    writer.close()
-
 async def main():
     # parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=6379)
+    parser.add_argument('--replicaof', type=str)
     args = parser.parse_args()
 
     HOST = "localhost"
     PORT = args.port
+    REPLICAOF = args.replicaof
+
+    async def handle_client(reader, writer):
+        store = Store()
+        while True:
+            data = await reader.read(100)
+            if not data:
+                break
+            to_send, store = command_dispatcher(data, store, REPLICAOF)
+            writer.write(to_send)
+            await writer.drain()
+        writer.close()
+
     server = await asyncio.start_server(handle_client, HOST, PORT)
     async with server:
         await server.serve_forever()
